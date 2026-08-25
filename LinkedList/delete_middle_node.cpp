92496571057f99ed3f6061968069056f20932075
Akash <akashrbllps.1444@gmail.com>
2026-08-26 01:05:02 +0530

    something done

---
+++ LinkedList/delete_middle_node.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+
+#define le(i, e) for (int i=0; i<e; i++)
+#define lse(i, s, e) for (int i=s; i<e; i++)
+#define pb push_back
+
+using namespace std;
+
+struct Node {
+    int data;
+    Node* next;
+
+    Node(int value){
+        data = value;
+        next = nullptr;
+    }
+};
+
+Node* array_to_ll(int* arr, int size){
+    Node* head = new Node(*arr);
+    Node* current = head;
+    lse(i, 1, size){
+        Node* newnode = new Node(*(arr+i));
+        current->next = newnode;
+        current = current->next;
+    }
+    return head;
+}
+
+void trans(Node* head){
+    Node* current = head;
+    while(current!=nullptr){
+        cout << current->data << " ";
+        current = current->next;
+    }
+}
+
+int main(){
+    int a, b, x=0, n;
+    if(!(cin >> n)) return 0;
+    int arr[n];
+    le(i, n) cin >> *(arr+i);
+
+    Node* head = array_to_ll(arr, n);
+
+    Node* current = head;
+
+    while(current != nullptr) {current = current->next; x++;}
+    current = head;
+    le(i, (int) ((x-1)/2)) current = current->next;
+    current->next = current->next->next;
+    
+    trans(head);
+}
